9b31ae61a3aeb3920df690cf8fd9fd9ee8341326
Cripto Coke <novasxsac@gmail.com>
2025-12-13 16:48:47 -0300

    [dyad] Increasing WhatsApp button background opacity to 40% - wrote 1 file(s)

--- src/components/whatsapp-button.tsx
+++ src/components/whatsapp-button.tsx
@@ -38,7 +38,7 @@ export const WhatsappButton = () => {
         mobileOffset
       )}
     >
-      <div className="relative h-14 w-14 md:h-16 md:w-16 shadow-xl rounded-full overflow-hidden hover:scale-105 transition-transform bg-green-500/20">
+      <div className="relative h-14 w-14 md:h-16 md:w-16 shadow-xl rounded-full overflow-hidden hover:scale-105 transition-transform bg-green-500/40">
         <Image
           src="/images/whatsapp-sushiaki.png"
           alt="WhatsApp Sushiaki"
